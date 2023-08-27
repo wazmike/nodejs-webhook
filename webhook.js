6748b534374f6ce360cb3d771d5d19cb7acb5f33
wazmike <30406214+wazmike@users.noreply.github.com>
2023-08-27 23:21:19 +0500

    Update webhook.js

--- webhook.js
+++ webhook.js
@@ -1,7 +1,13 @@
 'use strict';
 module.exports = (webhook) => {
   webhook.on('price', (session) => {
-            session.ans = session.price * 22;
+        if (session.kolvo < session.dostupno) {
+          session.ans = (session.price + 500) * session.kolvo;
+        }
+        else {
+          session.ans = "Такого количества на складе нет"
+        }
+            
         }
     );
 }
