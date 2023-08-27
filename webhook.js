ba676bf052cdd14a841ccf4971b68a681a4ec7e6
wazmike <30406214+wazmike@users.noreply.github.com>
2023-08-27 20:17:12 +0500

    Update webhook.js

--- webhook.js
+++ webhook.js
@@ -1,17 +1,7 @@
-/**
- * Once your bot steps into the screen with webhook action enabled, it requests your webhook URL with a full set of currently available variables.
- * Here you can add handlers for such actions to calculate any data for your bot.
- * Every handler is a simple function that accepts an object with variables from your bot and can modify it.
- * Once the handler has modified or added some variables, the bot receives them and continues to execute blocks in scenario.
- * Please read more here https://github.com/aimylogic/nodejs-webhook
- */
-
 'use strict';
-
 module.exports = (webhook) => {
-    /**
-     * Register some handlers here
-     *
-     * webhook.on('action1', (session) => session.variable = 'some value');
-     */
-};
+  webhook.on('price', (session) => {
+            ans = session.price * 22;
+        }
+    );
+}
